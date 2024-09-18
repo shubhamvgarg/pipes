72479951ba3701edd3f1f967ba859ec5834dc440
Shubham Garg <shubham07119garg@gmail.com>
2024-09-18 21:01:39 +0530

    Adding footer in app.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Contact from './components/Contact/Contact';
 import About from './components/About/About';
 import Product from './components/Product/Product';
 import Quality from './components/Quality/Quality';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
+        <Route path='/' element={<Home/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/about-us' element={<About/>}/>
         <Route path='/product' element={<Product/>}/>
         <Route path='/quality' element={<Quality/>}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
