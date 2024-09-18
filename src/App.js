849272ba62666b4ca6f8f5aaa8ea11e081518643
Shubham Garg <shubham07119garg@gmail.com>
2024-09-18 13:25:33 +0530

    updating App.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Header from './components/Header/Header';
+import Contact from './components/Contact/Contact';
+import About from './components/About/About';
+import Product from './components/Product/Product';
+import Quality from './components/Quality/Quality';
 
 function App() {
   return (
-   
-    <div className="App">
-      <Header />
-    </div>
-   
-  
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/about-us' element={<About/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/quality' element={<Quality/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
